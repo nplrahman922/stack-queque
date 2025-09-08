6b6c3d6010680db373b0e473e041ee694535d7b3
ASUS <nplrahma922@gmail.com>
2025-09-08 21:34:34 +0800

    update swap pada stack

--- src/main/java/org/example/Stack.java
+++ src/main/java/org/example/Stack.java
@@ -51,7 +51,7 @@ public class Stack {
         this.first = second;
     }
 
-    public int swap_limited_edition(char newValue) {
+    public char swap_limited_edition(char newValue) {
 
         if (first == null) {
             throw new RuntimeException("Stack kosong, tidak ada elemen untuk diganti.");
@@ -66,6 +66,57 @@ public class Stack {
         this.first = newNode;
 
         return oldValue;
+    }
+
+    public void swap_limited2(int p1, int p2) {
+
+        int pos1 = Math.min(p1, p2);
+        int pos2 = Math.max(p1, p2);
+
+        if (pos1 <= 0) {
+            System.out.println("ERROR: Posisi harus bilangan positif.");
+            return;
+        }
+
+        if (pos1 == pos2) {
+            System.out.println("INFO: Posisi sama, tidak ada yang ditukar.");
+            return;
+        }
+
+        Node prev1 = null;
+        Node node1 = first;
+        for (int i = 1; i < pos1 && node1 != null; i++) {
+            prev1 = node1;
+            node1 = node1.getNext();
+        }
+
+        Node prev2 = null;
+        Node node2 = first;
+        for (int i = 1; i < pos2 && node2 != null; i++) {
+            prev2 = node2;
+            node2 = node2.getNext();
+        }
+
+        if (node1 == null || node2 == null) {
+            System.out.println("ERROR: Posisi di luar jangkauan stack.");
+            return;
+        }
+
+        System.out.println("SWAP: Menukar nilai '" + node1.getValue() + "' (posisi " + pos1 + ") dengan '" + node2.getValue() + "' (posisi " + pos2 + ").");
+
+        if (prev1 != null) {
+            prev1.setNext(node2);
+        } else {
+            this.first = node2;
+        }
+
+        if (prev2 != null) {
+            prev2.setNext(node1);
+        }
+
+        Node temp = node1.getNext();
+        node1.setNext(node2.getNext());
+        node2.setNext(temp);
     }
 
     public void display() {
